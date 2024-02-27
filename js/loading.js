ff52d434031459bd6ff840f4b05c6b8534938719
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-02-27 18:00:24 +0000

    Tue, Feb 27, 2024, 10:00 AM -08:00

--- js/loading.js
+++ js/loading.js
@@ -1,5 +1,17 @@
 loadText = document.querySelector('.loading-text')
 
+bg = document.querySelector('.bg')
 load = 0
 
-loadText.innerText = "8%"
+int = setInterval(blurring, 30)
+
+
+function blurring( ) {
+    load++
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerText = `${load}%`
+    loadText.style.opacity = 1
+    bg.style.filter = "blur(10px)"
+}
