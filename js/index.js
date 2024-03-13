40cd03e19bdd7b40ff3364ecec761b27bc7dff3f
Isaiah Hernandez <142829094+Walmart-Warrior@users.noreply.github.com>
2024-03-13 16:51:30 +0000

    Wed, Mar 13, 2024, 9:51 AM -07:00

--- js/index.js
+++ js/index.js
@@ -2,5 +2,7 @@ var urlMenu = document.getElementById('websites')
 
 urlMenu.onchange = function() {
     var userOption = this.options[this.selectedIndex]
-    
+    if (userOption.value != "nothing") {
+        window.open(user)
+    }
 }
